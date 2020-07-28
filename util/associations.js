acb5334cf7ce0cef75be46631f33838575d49e94
mabutalid1 <marcobutalid989@gmail.com>
2020-07-28 16:21:02 +0800

    +paybases

--- util/associations.js
+++ util/associations.js
@@ -1,8 +1,10 @@
 const Customer = require('../models/customer');
 const Spouse = require('../models/spouse');
 const Application = require('../models/application');
+const Paybases = require('../models/payment_classifications');
 
 const association = function(){
+  Paybases;
   Spouse.belongsTo(Customer);
   Customer.hasOne(Spouse);
   Customer.hasMany(Application);
--- util/sync.js
+++ util/sync.js
@@ -1,5 +1,6 @@
 const sequelize = require('./database');
 const associations = require('./associations');
+const Paybases = require('../models/payment_classifications');
 
 associations();
 
@@ -7,6 +8,25 @@ associations();
 sequelize
 .sync()
 // .sync({force:true})
+.then(() => {
+    Paybases.bulkCreate([
+        {
+            pay_type:'DAILY',
+            interest_rate:0.14,
+            due_interest_rate: 0.7
+        },
+        {
+            pay_type:'WEEKLY',
+            interest_rate:0.1,
+            due_interest_rate: 0.7
+        },
+        {
+            pay_type:'MONTHLY',
+            interest_rate:0.1,
+            due_interest_rate: 0.7
+        }
+    ])
+})
 .then(() => {
     throw "Successfuly synced";
 })
