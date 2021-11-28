61d7b69ca298aede2223b4533b7782ebb63e8d98
mabutalid <theoaguilar123@gmail.com>
2021-11-29 00:04:42 +0800

    feat(SP-40): added authority checking in cutoff period

--- middleware/authentication/cutOff.js
+++ middleware/authentication/cutOff.js
@@ -1,14 +1,22 @@
 const momentZone = require('moment-timezone');
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 
 const Logger = require('../../utility/logger');
 
 require('dotenv').config();
+
+const verifyToken = promisify(jwt.verify).bind(jwt);
+
+// JWT secret
+const accessTokenSecret = process.env.JWT_ACCESS_TOKEN_SECRET;
+
 const cutOffStartPeriodHour = parseInt(process.env.START_HOUR, 10);
 const cutOffStartPeriodMinute = parseInt(process.env.START_MINUTE, 10);
 const cutOffEndPeriodHour = parseInt(process.env.END_HOUR, 10);
 const cutOffEndPeriodMinute = parseInt(process.env.END_MINUTE, 10);
 
-exports.CheckCutoff = (_req, _res, next) => {
+exports.CheckCutoff = async (req, res, next) => {
     Logger.info('=====[Function - CheckCutoff]=====');
     Logger.info('Checking Cutoff Validity');
     const currentTimeZone = momentZone.tz('Asia/Manila');
@@ -16,22 +24,33 @@ exports.CheckCutoff = (_req, _res, next) => {
     const currentHour = currentTimeZone.hour();
     const currentMinute = currentTimeZone.minute();
 
-    if (currentHour < cutOffStartPeriodHour) {
-        Logger.info('Access Denied - current time does not meet the cut off start period - hour stage');
-        throw { authenticated: false, message: 'ACCESS DENIED - WAIT FOR 8am' };
+    if (!req.signedCookies || !req.signedCookies.accessToken) {
+        Logger.info('Access Denied - User not authenticated');
+        return res.status(403).json({ error: { authenticated: false, message: 'ACCESS DENIED - User not authenticated' } });
     }
-    else if (currentHour === cutOffStartPeriodHour && currentMinute < cutOffStartPeriodMinute) {
-        Logger.info('Access Denied - current time does not meet the cut off start period - minute stage');
-        throw { authenticated: false, message: 'ACCESS DENIED - WAIT FOR 8am' };
-    }
-        
-    if (currentHour > cutOffEndPeriodHour) {
-        Logger.info('Access Denied - current time exceeds the cut off end period - hour stage');
-        throw { authenticated: false, message: 'ACCESS DENIED - ALREADY BEYOND 7pm' };
-    }
-    else if (currentHour === cutOffEndPeriodHour && currentMinute >= cutOffEndPeriodMinute) {
-        Logger.info('Access Denied - current time exceeds the cut off end period - minute stage');
-        throw { authenticated: false, message: 'ACCESS DENIED - ALREADY BEYOND 7pm' };
+
+    const accessToken = req.signedCookies.accessToken;
+
+    const verificationResult = await verifyToken(accessToken, accessTokenSecret, { ignoreExpiration: true });
+
+    if (verificationResult.authority !== 1) {
+        if (currentHour < cutOffStartPeriodHour) {
+            Logger.info('Access Denied - current time does not meet the cut off start period - hour stage');
+            return res.status(403).json({ error: { authenticated: false, message: 'ACCESS DENIED - WAIT FOR 8am' } });
+        }
+        else if (currentHour === cutOffStartPeriodHour && currentMinute < cutOffStartPeriodMinute) {
+            Logger.info('Access Denied - current time does not meet the cut off start period - minute stage');
+            return res.status(403).json({ error: { authenticated: false, message: 'ACCESS DENIED - WAIT FOR 8am' } });
+        }
+            
+        if (currentHour > cutOffEndPeriodHour) {
+            Logger.info('Access Denied - current time exceeds the cut off end period - hour stage');
+            return res.status(403).json({ error: { authenticated: false, message: 'ACCESS DENIED - ALREADY BEYOND 7pm' } });
+        }
+        else if (currentHour === cutOffEndPeriodHour && currentMinute >= cutOffEndPeriodMinute) {
+            Logger.info('Access Denied - current time exceeds the cut off end period - minute stage');
+            return res.status(403).json({ error: { authenticated: false, message: 'ACCESS DENIED - ALREADY BEYOND 7pm' } });
+        }
     }
 
     Logger.info('Access Allowed - Current Time valid for Cutoff Period');
